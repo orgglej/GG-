00fa21a49be1b08638d7461352cc26ffbe87aaba
orgglej <joselukas13209@gmail.com>
2024-08-23 15:14:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function clearResult() {
     document.getElementById('result').value = '';
 }
 
-function calculateResult(carregando...) {
+function calculateResult() {
     let result = eval(document.getElementById('result').value);
     document.getElementById('result').value = result;
 }
