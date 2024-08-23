72059bde4de37505f426179e9b83ca0b0531f942
orgglej <joselukas13209@gmail.com>
2024-08-23 15:11:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function clearResult() {
     document.getElementById('result').value = '';
 }
 
-function calculateResult("Ja existe um usuario coom esse calculo") {
+function calculateResult(carregando...) {
     let result = eval(document.getElementById('result').value);
     document.getElementById('result').value = result;
 }
